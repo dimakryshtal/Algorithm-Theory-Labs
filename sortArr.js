1a8a145c71875de920dde4f935e6f638fbf8679c
dimakryshtal <43169150+dimakryshtal@users.noreply.github.com>
2019-02-17 13:12:15 +0200

    Add files via upload

---
+++ sortArr.js
@@ -0,0 +1,33 @@
+const start = new Date();
+
+const sortArr = arr => {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 == 0) {
+      let x = arr[i];
+      let j = i;
+      while((j > 0 && arr[j - 1] >= x) || (j > 0 && arr[j - 1] % 2 != 0)) {
+        arr[j] = arr[j - 1];
+        j--;
+      }
+      arr[j] = x;
+    }
+  }
+
+  for(let i = arr.length - 1; i > 0; i--) {
+    if(arr[i] % 2 != 0) {
+      let x = arr[i];
+      let j = i;
+      while((j > 0 && arr[j + 1] >= x) || (j > 0 && arr[j + 1] % 2 === 0)) {
+        arr[j] = arr[j + 1];
+        j++;
+      }
+      arr[j] = x;
+    }
+  }
+  return arr;
+}
+const list = [30, 19, 9, 15, 55, 24, 3, 78, 46, 41];
+console.log(sortArr(list));
+
+const end = new Date();
+console.log(end - start);

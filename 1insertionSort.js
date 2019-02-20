2e2a2e3338cedcc44c72581b24b0798771376d85
dimakryshtal <43169150+dimakryshtal@users.noreply.github.com>
2019-02-20 14:35:26 +0200

    Update 1insertionSort.js

--- 1insertionSort.js
+++ 1insertionSort.js
@@ -26,8 +26,14 @@ const sortArr = arr => {
   }
   return arr;
 }
-const list = [30, 19, 9, 15, 55, 24, 3, 78, 46, 41];
-console.log(sortArr(list));
+
+const list1 = [30, 19, 9, 15, 55, 24, 3, 78, 46, 41];
+const list2 = [15 , 12, 43, 65, -1, 44, 32, -11, 11, 42, -8 , 14, -3, -8];
+const list3 = [12, 33, 21, 35, 34, -1, 3, 24,2 ,5, 88, 99,-2, 22, -15]
+
+console.log(sortArr(list1));  //[ 24, 30, 46, 78, 55, 41, 19, 15, 9, 3 ]
+console.log(sortArr(list2));  //[ -8, -8, 12, 14, 32, 42, 44, 65, 43, 15, 11, -1, -3, -11 ]
+console.log(sortArr(list3));  //[ -2, 2, 12, 22, 24, 34, 88, 99, 35, 33, 21, 5, 3, -1, -15 ]
 
 const end = new Date();
 console.log(end - start);
